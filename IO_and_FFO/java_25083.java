bca783c128d7840a768b26bb1abffffa5ac5df6e
yunii23 <67429918+yunii23@users.noreply.github.com>
2022-08-05 18:28:48 +0900

    백준 25083 완료

---
+++ IO_and_FFO/java_25083.java
@@ -0,0 +1,14 @@
+package algorithm2.IO_and_FFO;
+
+public class java_25083 {
+    // 백준 새싹
+
+    public static void main(String[] args) {
+        System.out.println("         ,r\'\"7");
+        System.out.println("r`-_   ,\'  ,/");
+        System.out.println(" \\. \". L_r\'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
