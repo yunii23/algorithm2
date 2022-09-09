84f7165fe09ccf4e397657ad680748793d4443b3
yunii23 <67429918+yunii23@users.noreply.github.com>
2022-09-09 20:36:05 +0900

    프로그래머스 레벨1 평균 구하기 완료

---
+++ Programmers/Level1/java_12944.java
@@ -0,0 +1,23 @@
+package algorithm2.Programmers.Level1;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
+public class java_12944 {
+    // 프로그래머스 레벨1 평균 구하기
+
+    public static void main(String[] args) {
+        int[] arr = {5,5};
+        double answer = 0;
+
+        int sum = 0;
+        for(int i=0; i<arr.length; i++) {
+            sum += arr[i];
+        }
+
+        DecimalFormat dr = new DecimalFormat("#.#");
+        answer = (double) sum / arr.length;
+
+        System.out.println(dr.format(answer));
+    }
+}
