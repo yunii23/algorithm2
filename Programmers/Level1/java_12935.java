62a765f946b9756922a888055fabdb16f074cde7
yunii23 <67429918+yunii23@users.noreply.github.com>
2022-09-10 20:09:58 +0900

    프로그래머스 레벨1 제일 작은 수 제거하기 완료

---
+++ Programmers/Level1/java_12935.java
@@ -0,0 +1,39 @@
+package algorithm2.Programmers.Level1;
+
+import java.util.Arrays;
+
+public class java_12935 {
+    // 프로그래머스 레벨1 제일 작은수 제거하기
+    static int[] answer;
+
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,1};
+
+        if(arr.length == 1) {
+            answer = new int[1];
+            answer[0] = -1;
+            System.out.println(answer[0]);
+        }
+        else {
+            int[] arrMin = Arrays.copyOf(arr, arr.length);
+            System.out.println(Arrays.toString(arr));
+            Arrays.sort(arrMin);
+            System.out.println(Arrays.toString(arrMin));
+            System.out.println(Arrays.toString(arr));
+
+            answer = new int[arr.length-1];
+
+            int count = 0;
+
+            for(int i=0; i<arr.length; i++) {
+                if(arr[i] != arrMin[0]) {
+                    answer[count] = arr[i];
+                    count++;
+                }
+            }
+
+            System.out.println(Arrays.toString(answer));
+        }
+        
+    }
+}
